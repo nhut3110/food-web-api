474ce1ba52137866f4ba117babfafc13d39bc40e
Nicole <nhut.vo200205@vnuk.edu.vn>
2022-11-22 13:16:10 +0700

    fix: seeder

--- database/seeders/TypeFoodSeeder.php
+++ database/seeders/TypeFoodSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class TypeFoodSeeder extends Seeder
 {
@@ -15,14 +16,21 @@ class TypeFoodSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
         DB::table('food_type')->insert([
             'name' => "Hoa quả",
+            'created_at'=>$faker->iso8601($max = 'now'),
+            'updated_at'=>$faker->iso8601($max = 'now')
         ]);
         DB::table('food_type')->insert([
             'name' => "Thực phẩm khô",
+            'created_at'=>$faker->iso8601($max = 'now'),
+            'updated_at'=>$faker->iso8601($max = 'now')
         ]);
         DB::table('food_type')->insert([
             'name' => "Rau hữu cơ",
+            'created_at'=>$faker->iso8601($max = 'now'),
+            'updated_at'=>$faker->iso8601($max = 'now')
         ]);
     }
 }
